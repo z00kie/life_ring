ccb8f3786507df0aec32cb6545fe4272dd7dfe76
Emmy White <emmalinerosewhite@gmail.com>
2018-03-27 15:10:33 -0400

    Completed full stub, created notes page.

--- lib/life_ring/cli.rb
+++ lib/life_ring/cli.rb
@@ -16,6 +16,7 @@ class LifeRing::CLI
     puts " What can I help you with today?"
     topics
     menu
+    goodbye
   end
 
   def topics
@@ -35,7 +36,47 @@ class LifeRing::CLI
   end
 
   def  menu
-    puts "What can I help you with today? Please enter a number 1-10 or enter 'more' for more options:"
+    input = nil
+    while input != "exit"
+      puts " "
+      puts "What can I help you with today? Please enter a menu number to see hotlines, 'menu' for the main menu, or 'exit' to leave."
+      puts " "
+
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts " "
+        puts "-----------------"
+        puts " "
+        puts "..ABORTION.."
+        puts " "
+        puts "(###)###-#### - Abortion Group"
+        puts "(###)###-#### - Abortion Support"
+        puts "(###)###-#### - Abortion Society"
+        puts " "
+        puts "-----------------"
+      when "2"
+        puts " "
+        puts "-----------------"
+        puts " "
+        puts "..ABUSE.."
+        puts " "
+        puts "(###)###-#### - Abuse Group"
+        puts "(###)###-#### - Abuse Support"
+        puts "(###)###-#### - Abuse Society"
+        puts " "
+        puts "-----------------"
+      when "menu"
+        topics
+      else
+        puts "I'm sorry, I didn't understand. Please type 'menu' or 'exit'."
+      end
+    end
+  end
+
+  def goodbye
+    puts " "
+    puts "Thank you for using Life Ring. We wish you a happy and healthy future!"
     puts " "
   end
 
---
+++ notes.md
@@ -0,0 +1,9 @@
+Life Ring is a database for US based hotlines for those who need help.
+
+Flow
+  --> Main menu/opening
+    --> Shows topics to request help with
+  --> Asks user for input
+  --> Displays all hotlines within that topic
+  --> Asks user if they want to view the menu or exit
+  --> Cycles back through or exits
