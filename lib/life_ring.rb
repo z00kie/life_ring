469ae152a4529b3276463a7f05fbf881cdbff697
Emmy White <emmalinerosewhite@gmail.com>
2018-03-28 10:45:23 -0400

    Working on self.topics.

--- lib/life_ring.rb
+++ lib/life_ring.rb
@@ -2,6 +2,6 @@ require_relative "./life_ring/version"
 require_relative "./life_ring/topic"
 require_relative "./life_ring/cli"
 
-require "nokogiri"
 require "open-uri"
+require "nokogiri"
 require "pry"
--- lib/life_ring/topic.rb
+++ lib/life_ring/topic.rb
@@ -2,10 +2,12 @@ class LifeRing::Topic
   attr_accessor :name
   @@all = []
 
-  def self.topics(url="www.pleaselive.org/hotlines")
+  def self.topics(url="http://www.pleaselive.org/hotlines")
     # Scrapes website for data
-    doc = Nokogiri::HTML.css(open(url))
+    doc = Nokogiri::HTML(open(url))
     # Assigns data to variable(s)
+    doc.css(".red-right-border").each do |topic|
+
     binding.pry
 
     topic = self.new
