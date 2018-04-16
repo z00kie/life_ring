036127c8bbf83d4cfd6e266e7b7d40c746c69ac9
Emmy White <emmalinerosewhite@gmail.com>
2018-04-16 11:58:36 -0400

    Working on scraper.

--- lib/life_ring/topic.rb
+++ lib/life_ring/topic.rb
@@ -5,11 +5,10 @@ class LifeRing::Topic
   def self.topics
     # Scrapes website for data
     doc = Nokogiri::HTML(open("http://www.pleaselive.org/hotlines"))
-    binding.pry
     # Assigns data to variable(s)
-    doc.css(".vc_row.wpb_row.vc_row-fluid:nth-child(2)").each.with_index(1) do |topic, index|
+    doc.css(".vc_row.wpb_row.vc_row-fluid:nth-child(2)").each.with_index(1) do |title, index|
       topic = self.new
-      topic.name = topic.css(".red-right-border").text
+      topic.name = title.css(".red-right-border").text
 
       puts "#{index}. #{topic.name}"
     end
