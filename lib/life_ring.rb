7a5afde454f0b1b7e648b27026cf59b5f2748af0
Emmy White <emmalinerosewhite@gmail.com>
2018-04-16 11:22:51 -0400

    Working on scraper.

--- lib/life_ring.rb
+++ lib/life_ring.rb
@@ -1,6 +1,6 @@
 require_relative "./life_ring/version"
-require_relative "./life_ring/topic"
 require_relative "./life_ring/cli"
+require_relative "./life_ring/topic"
 
 require "open-uri"
 require "nokogiri"
--- lib/life_ring/topic.rb
+++ lib/life_ring/topic.rb
@@ -2,13 +2,17 @@ class LifeRing::Topic
   attr_accessor :name
   @@all = []
 
-  def self.topics(url="http://www.pleaselive.org/hotlines")
+  def self.topics
     # Scrapes website for data
-    doc = Nokogiri::HTML(open(url))
-    # Assigns data to variable(s)
-    doc.css(".red-right-border").each do |topic|
-
+    doc = Nokogiri::HTML(open("http://www.pleaselive.org/hotlines"))
     binding.pry
+    # Assigns data to variable(s)
+    doc.css(".vc_row.wpb_row.vc_row-fluid:nth-child(2)").each.with_index(1) do |topic, index|
+      topic = self.new
+      topic.name = topic.css(".red-right-border").text
+
+      puts "#{index}. #{topic.name}"
+    end
 
     topic = self.new
     topic.name = "Hotline 1"
