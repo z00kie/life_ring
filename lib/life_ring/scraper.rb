75b010fd52f99a9212e5fd88d78c6f867ce2a46b
Emmy White <emmalinerosewhite@gmail.com>
2018-04-17 12:05:31 -0400

    Getting Scraper.topics to work. Maybe.

--- lib/life_ring/scraper.rb
+++ lib/life_ring/scraper.rb
@@ -6,15 +6,15 @@ class LifeRing::Scraper
     @@all << self
   end
 
-  def topics
+  def self.topics
     # Scrapes website for data
     doc = Nokogiri::HTML(open("http://www.pleaselive.org/hotlines"))
     # Assigns data to variable(s)
       doc.css(".vc_row.wpb_row.vc_row-fluid:nth-child(2)").each do |post|
-        @topic = self.new
-        @topic.name = post.css("h2").text
-        @topic.organization = post.css(".red-text").text
-        @topic.phone = post.css(".blue-text").text
+        topic = self.new
+        topic.name = post.css("h2").text
+        topic.organization = post.css(".red-text").text
+        topic.phone = post.css(".blue-text").text
       end
     end
 
