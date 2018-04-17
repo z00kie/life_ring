13484084f6c73734cf6fa2f9ddf46dbaefb24792
Emmy White <emmalinerosewhite@gmail.com>
2018-04-17 12:28:02 -0400

    Updated environment require file.

--- lib/life_ring/topic.rb
+++ lib/life_ring/topic.rb
@@ -1,5 +1,17 @@
 class Topic
   attr_accessor :name, :organization, :phone
 
+  @@all = []
 
+  def initialize
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
 end
