a76c9fd7852b344a14cee1af43c6dcb7cc7b4b93
Emmy White <emmalinerosewhite@gmail.com>
2018-04-17 11:55:03 -0400

    Rebuilt scraper.

--- lib/life_ring/topic.rb
+++ lib/life_ring/topic.rb
@@ -1,19 +1,21 @@
 class LifeRing::Scraper
-  attr_accessor :name
+  attr_accessor :name, :organization, :phone
   @@all = []
 
-  def self.topics
+  def initialize
+    @@all << self
+  end
+
+  def topics
     # Scrapes website for data
     doc = Nokogiri::HTML(open("http://www.pleaselive.org/hotlines"))
     # Assigns data to variable(s)
       doc.css(".vc_row.wpb_row.vc_row-fluid:nth-child(2)").each do |post|
-        topic = self.new
-        topic.title = post.css("h2").text
-        topic.schedule = post.css(".date").text
-        topic.description = post.css("p").text
+        @topic = self.new
+        @topic.name = post.css("h2").text
+        @topic.organization = post.css(".red-text").text
+        @topic.phone = post.css(".blue-text").text
       end
-
-      puts "#{index}. #{topic.name}"
     end
 
     topic = self.new
