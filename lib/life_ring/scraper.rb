8c64bae338d430976ac7b94ce5b58f7f5e7dd305
Emmy White <emmalinerosewhite@gmail.com>
2018-04-30 11:35:26 -0400

    @topic.name working, fixing @topic.url

--- lib/life_ring/scraper.rb
+++ lib/life_ring/scraper.rb
@@ -9,10 +9,10 @@ class LifeRing::Scraper
   end
 
   def self.main_menu_info
-      grab_page.css("a.button--struggle").collect do |info|
+      grab_page.css("div.im-struggling-grid a.button--struggle").collect do |info|
         @topic = Topic.new
         @topic.name = info.css("span.button--struggle__text").text
-        @topic.url = info.css("a.href")
+        @topic.url = info.css("a")
         binding.pry
       end
     end
